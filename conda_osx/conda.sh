f2537e015118800703be60f490b16c8b820f43fa
looooo <sppedflyer@gmail.com>
2019-03-06 16:05:46 +0100

    add additional dir "APP"

--- conda_osx/conda.sh
+++ conda_osx/conda.sh
@@ -1,6 +1,6 @@
-assume we have a working conda available
+# assume we have a working conda available
 conda create \
-    -p FreeCAD.app/Contents/Resources \
+    -p APP/FreeCAD.app/Contents/Resources \
     freecad calculix blas=*=openblas \
     --copy \
     --no-default-packages \
@@ -11,14 +11,14 @@ conda create \
 # activating the environment to install additional tools
 # this can be skipped if we create conda-packages from the pip-packages...
 # extract the version information
-source activate FreeCAD.app/Contents/Resources
+source activate APP/FreeCAD.app/Contents/Resources
 FreeCAD --console $TRAVIS_BUILD_DIR/conda/version.py
 pip install https://github.com/looooo/freecad_pipintegration/archive/master.zip
 source activate base
 
 # this will create a huge env. We have to find some ways to make the env smaller
 # deleting some packages explicitly? 
-conda remove -p FreeCAD.app/Contents/Resources --force -y \
+conda remove -p APP/FreeCAD.app/Contents/Resources --force -y \
     ruamel_yaml conda system tk json-c llvmdev \
     nomkl readline mesalib \
     curl gstreamer libtheora asn1crypto certifi chardet \
@@ -27,17 +27,19 @@ conda remove -p FreeCAD.app/Contents/Resources --force -y \
     cycler python-dateutil cryptography pyqt soqt wheel \
     requests libstdcxx-ng xz sqlite ncurses
 
-conda list -p FreeCAD.app/Contents/Resources
+conda list -p APP/FreeCAD.app/Contents/Resources
 
 # delete unnecessary stuff
-rm -rf FreeCAD.app/Contents/Resources/include
-find FreeCAD.app/Contents/Resources -name \*.a -delete
-mv FreeCAD.app/Contents/Resources/bin FreeCAD.app/Contents/Resources/bin_tmp
-mkdir FreeCAD.app/Contents/Resources/bin
-cp FreeCAD.app/Contents/Resources/bin_tmp/FreeCAD FreeCAD.app/Contents/Resources/bin/FreeCAD
-cp FreeCAD.app/Contents/Resources/bin_tmp/FreeCAD FreeCAD.app/Contents/Resources/bin/
-cp FreeCAD.app/Contents/Resources/bin_tmp/python FreeCAD.app/Contents/Resources/bin/
-cp FreeCAD.app/Contents/Resources/bin_tmp/pip FreeCAD.app/Contents/Resources/bin/
-cp FreeCAD.app/Contents/Resources/bin_tmp/pyside2-rcc FreeCAD.app/Contents/Resources/bin/
-sed -i "" '1s|.*|#!/usr/bin/env python|' FreeCAD.app/Contents/Resources/bin/pip
-rm -rf FreeCAD.app/Contents/Resources/bin_tmp
+rm -rf APP/FreeCAD.app/Contents/Resources/include
+find APP/FreeCAD.app/Contents/Resources -name \*.a -delete
+mv APP/FreeCAD.app/Contents/Resources/bin APP/FreeCAD.app/Contents/Resources/bin_tmp
+mkdir APP/FreeCAD.app/Contents/Resources/bin
+cp APP/FreeCAD.app/Contents/Resources/bin_tmp/FreeCAD APP/FreeCAD.app/Contents/Resources/bin/FreeCAD
+cp APP/FreeCAD.app/Contents/Resources/bin_tmp/FreeCAD APP/FreeCAD.app/Contents/Resources/bin/
+cp APP/FreeCAD.app/Contents/Resources/bin_tmp/python APP/FreeCAD.app/Contents/Resources/bin/
+cp APP/FreeCAD.app/Contents/Resources/bin_tmp/pip APP/FreeCAD.app/Contents/Resources/bin/
+cp APP/FreeCAD.app/Contents/Resources/bin_tmp/pyside2-rcc APP/FreeCAD.app/Contents/Resources/bin/
+sed -i "" '1s|.*|#!/usr/bin/env python|' APP/FreeCAD.app/Contents/Resources/bin/pip
+rm -rf APP/FreeCAD.app/Contents/Resources/bin_tmp
+
+hdiutil create -volname FreeCAD_0.18-16027-OSX-x86_64-Qt5-Py3 -srcfolder ./APP -ov -format UDZO FreeCAD_0.18-16027-OSX-x86_64-Qt5-Py3.dmg
