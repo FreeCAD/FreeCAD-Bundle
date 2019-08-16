8cde9476d0d038c7754ad193f5c05067ce505859
looooo <sppedflyer@gmail.com>
2019-08-16 14:49:50 +0200

    switch back to qt5.9

--- conda/linux_dev/linux_dev.sh
+++ conda/linux_dev/linux_dev.sh
@@ -3,7 +3,7 @@ conda create \
     -p AppDir/usr \
     freecad calculix blas=*=openblas gitpython \
     numpy matplotlib-base scipy sympy pandas six pyyaml \
-    opencamlib ifcopenshell qt=5.12 \
+    opencamlib ifcopenshell qt=5.9 \
     --copy \
     -c freecad/label/dev \
     -c conda-forge \
