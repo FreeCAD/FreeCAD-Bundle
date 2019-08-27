683d77eb03d5c82a09e7f5c9829cfea73d9ad77c
looooo <sppedflyer@gmail.com>
2019-08-27 18:21:31 +0200

    conda: update to qt5.12 and add solvespace library

--- conda/linux_dev/linux_dev.sh
+++ conda/linux_dev/linux_dev.sh
@@ -2,8 +2,8 @@
 conda create \
     -p AppDir/usr \
     freecad calculix blas=*=openblas gitpython \
-    numpy matplotlib-base scipy sympy pandas six pyyaml \
-    opencamlib ifcopenshell qt=5.9 \
+    numpy matplotlib-base scipy sympy pandas six pyyaml solvespace\
+    opencamlib ifcopenshell qt=5.12 \
     --copy \
     -c freecad/label/dev \
     -c conda-forge \
