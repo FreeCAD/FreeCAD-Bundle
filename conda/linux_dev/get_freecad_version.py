55436d17ed153e4ad1d0e854f1a83ea5580eb514
looooo <sppedflyer@gmail.com>
2020-02-19 16:00:18 +0100

    Revert "add versions of python, qt, occt to filename of bundle"
    This reverts commit 2c5d0f8252fd9645b31749ae6bd78f51f1ef5977.

--- conda/linux_dev/get_freecad_version.py
+++ conda/linux_dev/get_freecad_version.py
@@ -3,17 +3,6 @@ import os
 import subprocess
 import platform
 
-def get_version(package_name):
-    """
-    returns the version given by conda list as a list of numbers (str)
-    """
-    proc = subprocess.Popen(["conda", "list", package_name],
-                            stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, err = proc.communicate()
-    out = out.decode("utf-8")
-    version = out.split("\n{} ".format(package_name))[1].split()[0]
-    return version.split(".")
-
 platform_dict = {}
 platform_dict["Darwin"] = "OSX"
 
@@ -28,9 +17,4 @@ version_info = version_info.decode("utf-8").split(" ")
 dev_version = version_info[1]
 revision = version_info[3]
 
-python_version = "".join(get_version("python")[:2])  # eg 37
-qt_version = "".join(get_version("qt")[:2])          # eg 512
-occt_version = "".join(get_version("occt")[:2])      # eg 73
-
-print("FreeCAD_{}-{}-{}-Conda_Py{}-Qt{}-Occt{}-glibc2.12-x86_64".format(dev_version, revision, system,
-                                                                        python_version, qt_version, occt_version))
+print("FreeCAD_{}-{}-{}-Conda_Py3Qt5_glibc2.12-x86_64".format(dev_version, revision, system))
