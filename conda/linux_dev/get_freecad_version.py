2c5d0f8252fd9645b31749ae6bd78f51f1ef5977
looooo <sppedflyer@gmail.com>
2020-02-19 15:27:47 +0100

    add versions of python, qt, occt to filename of bundle

--- conda/linux_dev/get_freecad_version.py
+++ conda/linux_dev/get_freecad_version.py
@@ -3,6 +3,17 @@ import os
 import subprocess
 import platform
 
+def get_version(package_name):
+    """
+    returns the version given by conda list as a list of numbers (str)
+    """
+    proc = subprocess.Popen(["conda", "list", package_name],
+                            stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+    out = out.decode("utf-8")
+    version = out.split("\n{} ".format(package_name))[1].split()[0]
+    return version.split(".")
+
 platform_dict = {}
 platform_dict["Darwin"] = "OSX"
 
@@ -17,4 +28,9 @@ version_info = version_info.decode("utf-8").split(" ")
 dev_version = version_info[1]
 revision = version_info[3]
 
-print("FreeCAD_{}-{}-{}-Conda_Py3Qt5_glibc2.12-x86_64".format(dev_version, revision, system))
+python_version = "".join(get_version("python")[:2])  # eg 37
+qt_version = "".join(get_version("qt")[:2])          # eg 512
+occt_version = "".join(get_version("occt")[:2])      # eg 73
+
+print("FreeCAD_{}-{}-{}-Conda_Py{}-Qt{}-Occt{}-glibc2.12-x86_64".format(dev_version, revision, system,
+                                                                        python_version, qt_version, occt_version))
--- conda/osx_dev/get_freecad_version.py
+++ conda/osx_dev/get_freecad_version.py
@@ -4,6 +4,19 @@ import subprocess
 import platform
 import jinja2
 
+
+def get_version(package_name):
+    """
+    returns the version given by conda list as a list of numbers (str)
+    """
+    proc = subprocess.Popen(["conda", "list", package_name],
+                            stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+    out = out.decode("utf-8")
+    version = out.split("\n{} ".format(package_name))[1].split()[0]
+    return version.split(".")
+
+
 platform_dict = {}
 platform_dict["Darwin"] = "OSX"
 
@@ -18,6 +31,10 @@ version_info = version_info.decode("utf-8").split(" ")
 dev_version = version_info[1]
 revision = version_info[3]
 
+python_version = "".join(get_version("python")[:2])  # eg 37
+qt_version = "".join(get_version("qt")[:2])          # eg 512
+occt_version = "".join(get_version("occt")[:2])      # eg 73
+
 with open("Info.plist.template") as template_file:
     template_str = template_file.read()
 template = jinja2.Template(template_str)
@@ -26,4 +43,5 @@ rendered_str = template.render(FREECAD_VERSION="{}-{}".format(dev_version, revis
 with open("APP/FreeCAD.app/Contents/Info.plist", "w") as rendered_file:
     rendered_file.write(rendered_str)
 
-print("FreeCAD_{}-{}-{}-{}-conda-Qt5-Py3".format(dev_version, revision, system, arch))
+print("FreeCAD_{}-{}-{}-{}-conda-Py{}-Qt{}-Occt{}".format(dev_version, revision, system, arch,
+                                                          python_version, qt_version, occt_version))
