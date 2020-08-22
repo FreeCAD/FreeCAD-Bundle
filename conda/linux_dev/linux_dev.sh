41ebeb8681eff9604d0584c3586e95cec78d0711
looooo <sppedflyer@gmail.com>
2020-08-22 11:01:07 +0200

    add pythonocc-core

--- conda/linux_dev/linux_dev.sh
+++ conda/linux_dev/linux_dev.sh
@@ -2,7 +2,7 @@
 conda create \
     -p AppDir/usr \
     freecad calculix blas=*=openblas gitpython netgen=6.2.1808 \
-    numpy matplotlib-base scipy=1.4.1 sympy pandas six \
+    numpy matplotlib-base scipy=1.4.1 sympy pandas pythonocc-core six \
     pyyaml opencamlib ifcopenshell boost-cpp=1.72 \
     freecad.asm3 libredwg pycollada appimage-updater-bridge \
     lxml xlutils olefile requests \
