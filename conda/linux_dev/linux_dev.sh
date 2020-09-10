9896f02e16306951df001d75870059ba23892ca6
looooo <sppedflyer@gmail.com>
2020-09-10 11:27:56 +0200

    remove ifcopenshell, needs to be updated to icu67: https://github.com/conda-forge/ifcopenshell-feedstock/pull/10

--- conda/linux_dev/linux_dev.sh
+++ conda/linux_dev/linux_dev.sh
@@ -3,7 +3,7 @@ conda create \
     -p AppDir/usr \
     freecad calculix blas=*=openblas gitpython netgen=6.2.1808 \
     numpy matplotlib-base scipy=1.4.1 sympy pandas pythonocc-core six \
-    pyyaml opencamlib ifcopenshell boost-cpp=1.72 \
+    pyyaml opencamlib boost-cpp=1.72 \
     freecad.asm3 libredwg pycollada appimage-updater-bridge \
     lxml xlutils olefile requests \
     blinker opencv qt.py nine docutils \
--- conda/osx_dev/osx_dev.sh
+++ conda/osx_dev/osx_dev.sh
@@ -3,7 +3,7 @@ conda create \
     -p APP/FreeCAD.app/Contents/Resources \
     freecad calculix blas=*=openblas gitpython netgen=6.2.1808 \
     numpy matplotlib-base scipy=1.4.1 sympy pandas pythonocc-core six \
-    pyyaml jinja2 opencamlib ifcopenshell boost-cpp=1.72 \
+    pyyaml jinja2 opencamlib boost-cpp=1.72 \
     freecad.asm3 libredwg pycollada \
     lxml xlutils olefile requests \
     blinker opencv qt.py nine docutils \
