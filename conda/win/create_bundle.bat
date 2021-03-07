5d13c11b1f30c30798e7037189dceed6a4fee3f2
lorenz <looooo@users.noreply.github.com>
2021-03-08 00:57:22 +0100

    Update create_bundle.bat

--- conda/win/create_bundle.bat
+++ conda/win/create_bundle.bat
@@ -56,13 +56,16 @@ set /p fcver=<tempver.txt
 IF "%DEPLOY_RELEASE%"=="weekly-builds" (
     set fcver="weekly-builds"
 )
+ELSE (
+    set fcver=%fcver:~0,10%
+)
 echo %fcver%
 cd %copy_dir%\..
-ren %copy_dir% FreeCAD_%fcver:~0,10%-Win-Conda_vc14.x-x86_64
+ren %copy_dir% FreeCAD_%fcver%-Win-Conda_vc14.x-x86_64
 dir
 
 REM if errorlevel1 exit 1
 
-"%ProgramFiles%\7-Zip\7z.exe" a -t7z -mmt=%NUMBER_OF_PROCESSORS% FreeCAD_%fcver:~0,10%-Win-Conda_vc14.x-x86_64.7z FreeCAD_%fcver:~0,10%-Win-Conda_vc14.x-x86_64\ -bb
-certutil -hashfile "FreeCAD_%fcver:~0,10%-Win-Conda_vc14.x-x86_64.7z" SHA256 > "FreeCAD_%fcver:~0,10%-Win-Conda_vc14.x-x86_64.7z"-SHA256.txt
-echo  %date%-%time% >>"FreeCAD_%fcver:~0,10%-Win-Conda_vc14.x-x86_64.7z"-SHA256.txt
+"%ProgramFiles%\7-Zip\7z.exe" a -t7z -mmt=%NUMBER_OF_PROCESSORS% FreeCAD_%fcver%-Win-Conda_vc14.x-x86_64.7z FreeCAD_%fcver%-Win-Conda_vc14.x-x86_64\ -bb
+certutil -hashfile "FreeCAD_%fcver%-Win-Conda_vc14.x-x86_64.7z" SHA256 > "FreeCAD_%fcver%-Win-Conda_vc14.x-x86_64.7z"-SHA256.txt
+echo  %date%-%time% >>"FreeCAD_%fcver%-Win-Conda_vc14.x-x86_64.7z"-SHA256.txt
