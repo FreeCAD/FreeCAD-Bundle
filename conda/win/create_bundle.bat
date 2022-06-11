e03c8cddba9019defca27c41fe3a972b425e7b3c
lorenz <looooo@users.noreply.github.com>
2022-06-11 02:06:37 +0200

    Update create_bundle.bat

--- conda/win/create_bundle.bat
+++ conda/win/create_bundle.bat
@@ -17,7 +17,7 @@ call mamba create ^
  -y
  
  
-conda run -p %conda_env% ..\scripts\get_freecad_version.py
+conda run -p %conda_env% python ..\scripts\get_freecad_version.py
 set /p freecad_version_name= <bundle_name.txt
 
 echo **********************
