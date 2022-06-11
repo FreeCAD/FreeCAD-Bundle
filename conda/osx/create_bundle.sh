b3ffd6a0827baab92af39c8a0a116327c12d122f
lorenz <looooo@users.noreply.github.com>
2022-06-11 02:03:43 +0200

    Update create_bundle.sh

--- conda/osx/create_bundle.sh
+++ conda/osx/create_bundle.sh
@@ -16,7 +16,7 @@ mamba create \
     -c conda-forge \
     -y
 
-mamba run -p ${env_dir} python ../scripts/get_freecad_version.py
+mamba run -p ${conda_ev} python ../scripts/get_freecad_version.py
 read -r version_name < bundle_name.txt
 
 echo -e "\################"
