d89047ef0001076b5e9a016a51626feb130ec89c
looooo <sppedflyer@gmail.com>
2022-08-03 15:42:35 +0200

    add windows 3.10 bundle

--- conda/win_py38/create_bundle.bat
+++ conda/win_py38/create_bundle.bat
@@ -5,7 +5,7 @@ mkdir %copy_dir%
 
 call mamba create ^
  -p %conda_env% ^
- freecad=*.pre python=3.10 occt=7.6 vtk=9 libredwg calculix gitpython gmsh ^
+ freecad=*.pre python=3.8 occt=7.6 vtk=9 libredwg calculix gitpython gmsh ^
  numpy matplotlib-base scipy sympy pandas six ^
  pyyaml opencamlib openglider ^
  libredwg pycollada lxml xlutils olefile requests ^
