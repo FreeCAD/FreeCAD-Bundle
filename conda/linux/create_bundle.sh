93b8f541cf8a1951b08d1bddffe1839afa91c853
Adrián Insaurralde Avalos <adrianinsaval@gmail.com>
2024-09-09 16:41:44 -0400

    1.0rc1 [skip ci]

--- conda/linux/create_bundle.sh
+++ conda/linux/create_bundle.sh
@@ -8,7 +8,7 @@ fi
 conda_env="AppDir/usr"
 echo -e "\nCreate the environment"
 
-packages="freecad=*.pre occt vtk python=3.11 blas=*=openblas numpy \
+packages="freecad=1.0rc1 occt vtk python=3.11 blas=*=openblas numpy \
           matplotlib-base scipy sympy pandas six pyyaml pycollada lxml \
           xlutils olefile requests blinker opencv nine docutils \
           opencamlib calculix ifcopenshell lark appimage-updater-bridge"
